ea3424085239e82329b59baafe9207d47a4e009c
Diego Tribek <diego.tribek@dasa.com.br>
2023-12-22 19:39:20 -0300

    changed query 3 using map_zip implementation

--- all_tracks/fact_modeling/query_3.sql
+++ all_tracks/fact_modeling/query_3.sql
@@ -9,12 +9,12 @@ WITH old_data AS (
 ),
   
 /*Get Data from current day*/
-day_data AS (
+new_data AS (
   SELECT
     web.user_id,
-    CAST(dev.browser_type as VARCHAR) as device_info,
-    DATE(web.event_time) as event_date
-
+    DATE(web.event_time) as event_date,
+    MAP_AGG(dev.browser_type, ARRAY[CAST(web.event_time AS date)]) AS device_activity_datelist
+        
   FROM bootcamp.web_events as web
   
   LEFT JOIN bootcamp.devices as dev
@@ -22,22 +22,7 @@ day_data AS (
   
   WHERE
   DATE(event_time) = DATE('2022-01-05')
-  GROUP BY 1, 2, 3
-)
-
-/*CTE to get all devices in array*/
-,new_data as (
-  SELECT 
-    new.user_id,
-    new.device_info,
-    new.event_date,
-    ARRAY_AGG(new_2.device_info) as device_array
-
-  FROM day_data new
-  INNER JOIN day_data new_2
-  ON new.user_id = new_2.user_id
-
-  GROUP BY 1,2,3
+  GROUP BY 1, 2
 )
 
 /*Aggregate old data with new data*/
@@ -52,42 +37,14 @@ day_data AS (
       
       /*New records*/
       WHEN old_data.user_id IS NULL AND new_data.user_id IS NOT NULL 
-        THEN MAP(ARRAY[new_data.device_info], ARRAY[ARRAY[new_data.event_date]])        
-  
+        THEN new_data.device_activity_datelist
 
-      /*Records will change*/
-      WHEN old_data.user_id IS NOT NULL AND new_data.user_id IS NOT NULL
-        THEN 
-           /*Check if the device is present in the list or not*/
-            CASE 
-                 /*Device is not present in the device list*/
-                WHEN element_at(old_data.device_activity_datelist, new_data.device_info) IS NULL
-                    THEN 
-                      /*If all other devices have data today, just map the current device*/ 
-                      CASE WHEN CARDINALITY(ARRAY_EXCEPT(map_keys(old_data.device_activity_datelist), new_data.device_array)) = 0
-                        THEN MAP(ARRAY[new_data.device_info], ARRAY[ARRAY[new_data.event_date]]) 
-                         /*Concat current device data with old devices data*/
-                         ELSE
-                            map_concat(MAP(ARRAY[new_data.device_info], ARRAY[ARRAY[new_data.event_date]]),
-                              map_filter(old_data.device_activity_datelist, 
-                              (k, v) -> CONTAINS(ARRAY_EXCEPT(map_keys(old_data.device_activity_datelist), new_data.device_array), k)
-                              )
-                            )      
-                      END
-                /*Device is present in the device list and every other device is in today's data*/
-                WHEN CARDINALITY(ARRAY_EXCEPT(map_keys(old_data.device_activity_datelist), new_data.device_array)) = 0
-                  THEN MAP(ARRAY[new_data.device_info], 
-                           ARRAY[DATE(new_data.event_date) || element_at(old_data.device_activity_datelist, new_data.device_info)])
-                  /*Deviceinfo is present in the device list but some device is not in today's data. 
-                  Concat the present device data with old data from devices that don't appear in current's date*/
-                  ELSE
-                    map_concat(MAP(ARRAY[new_data.device_info], 
-                      ARRAY[DATE(new_data.event_date) || element_at(old_data.device_activity_datelist, new_data.device_info)]),
-                      map_filter(old_data.device_activity_datelist, 
-                      (k, v) -> CONTAINS(ARRAY_EXCEPT(map_keys(old_data.device_activity_datelist), new_data.device_array), k)
-                      )
-                  )
-            END
+      ELSE
+          MAP_ZIP_WITH(
+                      COALESCE(old_data.device_activity_datelist, MAP()),
+                      COALESCE(new_data.device_activity_datelist, MAP()),
+                      (_, v1, v2) -> COALESCE(v1, ARRAY[]) || COALESCE(v2, ARRAY[])
+          )        
     END device_activity_datelist,
     DATE('2022-01-05') as event_date
   FROM old_data
